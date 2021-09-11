0b74e0945f1e790c10c1bfc870a56856d89359ce
BethanyJingLiu <86675901+BethanyJingLiu@users.noreply.github.com>
2021-09-11 22:09:06 +0200

    Learnt conditional render

--- src/App.js
+++ src/App.js
@@ -1,46 +1,87 @@
+import { useState } from "react";
 import Employee from "./Employee";
 import "./styles.css";
 
 export default function App() {
-  const employees = [
+  const employeesGroup1 = [
     {
-      id: "1",
+      id: "1-1",
       firstName: "Bethany",
       lastName: "Liu",
       age: "40"
     },
     {
-      id: "2",
+      id: "1-2",
       firstName: "Alex",
       lastName: "Kuleshov",
       age: "28"
     },
     {
-      id: "3",
+      id: "1-3",
       firstName: "Patricia",
       middleName: "Pinto",
       lastName: "Rodrigues",
       age: "25"
     },
     {
-      id: "4",
+      id: "1-4",
       firstName: "George",
       lastName: "Min",
       age: "60"
     }
   ];
+
+  const employeesGroup2 = [
+    {
+      id: "2-1",
+      firstName: "Victoria",
+      lastName: "Thomas",
+      age: "26"
+    },
+    {
+      id: "2-2",
+      firstName: "Bram",
+      lastName: "Jansen",
+      age: "26"
+    },
+    {
+      id: "2-3",
+      firstName: "Romy",
+      lastName: "Snel",
+      age: "25"
+    },
+    {
+      id: "2-4",
+      firstName: "Lisa",
+      lastName: "Bass",
+      age: "28"
+    }
+  ];
+
+  const employees = [...employeesGroup1, ...employeesGroup2];
+
+  const [loggedIn, setLoggedIn] = useState(false);
+
   return (
     <div className="App">
-      <h1>Employees</h1>
-      {employees.map((employee) => (
-        <Employee
-          key={employee.id}
-          firstName={employee.firstName}
-          middleName={employee.middleName}
-          lastName={employee.lastName}
-          id={employee.id}
-        />
-      ))}
+      <h1>Welcome to the App</h1>
+      <button onClick={() => setLoggedIn(!loggedIn)}>
+        {loggedIn ? "Log out" : "Log in"}
+      </button>
+      {loggedIn ? (
+        <>
+          {employees.map((employee) => (
+            <Employee
+              key={employee.id}
+              firstName={employee.firstName}
+              lastName={employee.lastName}
+              id={employee.id}
+            />
+          ))}
+        </>
+      ) : (
+        <h2>Sorry the employees are at home now</h2>
+      )}
     </div>
   );
 }
